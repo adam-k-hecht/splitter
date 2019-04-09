9ce00ba0844280c27ff85d03fd5a62fb46d2e6f4
Adam Hecht <adamhecht@BoostrProductTeam.fios-router.home>
2019-04-09 14:07:16 -0400

    splitter commit

---
+++ splitter.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+FILENAME=rubi_kiip_1H2018.csv
+HDR=$(head -1 ${FILENAME})
+split -l 1000 ${FILENAME} xyz
+n=1
+for f in xyz*
+do
+    if [[ ${n} -ne 1 ]]; then
+        echo ${HDR} > part-${n}-${FILENAME}.csv
+    fi
+    cat ${f} >> part-${n}-${FILENAME}.csv
+    rm ${f}
+    ((n++))
+done
